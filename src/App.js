762c8086da3a1d40f1e1bb91924018968f1f7d94
claudiamastan <claudiachen97@gmail.com>
2019-10-15 12:53:52 -0700

    implement clarifai. created face recognition component (static)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import 'tachyons';
+
+const app = new Clarifai.App({
+  apiKey: '32ee724d71b24bdfb6f9de591a055efa'
+});
 
 const particlesOption = {
   "particles": {
@@ -27,6 +33,35 @@ const particlesOption = {
 };
 
 class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('Click!');
+
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b",
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
+      function(response) {
+        // do something with response
+        console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+
+  }
+
   render(){
     return(
       <div className="App">
@@ -37,8 +72,11 @@ class App extends Component{
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />} */}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return(
         <div>
-            <p classname='f3'>
+            <p className='f3'>
                 {`This magic brain will detect faces in your pictures`}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' /><br/>
-                    <button className='w-30 grow f4 link ph3 pv dib white bg-light-purple tc'>Detect</button>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/><br/>
+                    <button 
+                        className='w-30 grow f4 link ph3 pv dib white bg-light-purple tc'
+                        onClick={onButtonSubmit}
+                    >
+                        Detect
+                    </button>
                 </div>
                 </div>
         </div>

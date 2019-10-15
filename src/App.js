167bb1ca4dbdfa936cc8be9b6f20ebbbe5e39227
claudiamastan <claudiachen97@gmail.com>
2019-10-15 10:44:26 -0700

    add rank component. add particles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from 'react-particles-js';
 import 'tachyons';
+
+const particlesOption = {
+  "particles": {
+    "number": {
+        "value": 100
+    },
+    "size": {
+        "value": 3
+    }
+  },
+  "interactivity": {
+    "events": {
+        "onhover": {
+            "enable": true,
+            "mode": "repulse"
+        }
+    }
+  }
+};
 
 class App extends Component{
   render(){
     return(
       <div className="App">
+        <Particles
+          className="particles" 
+          params={particlesOption}
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*<FaceRecognition />} */}
       </div>

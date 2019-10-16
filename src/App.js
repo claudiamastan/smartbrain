1b61581948c4e486618321b9b2c74ce5f64038f7
claudiamastan <claudiachen97@gmail.com>
2019-10-15 22:09:32 -0700

    add input & image url state. can display image based on input url. process url with clarifai api gives output

--- src/App.js
+++ src/App.js
@@ -36,24 +36,25 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('Click!');
-
-    app.models.predict(
-      "a403429f2ddf4b49b307e318f00e528b",
-      "https://samples.clarifai.com/face-det.jpg")
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
     .then(
       function(response) {
         // do something with response
-        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       },
       function(err) {
         // there was an error
@@ -76,7 +77,7 @@ class App extends Component{
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
